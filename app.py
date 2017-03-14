ef8e9d7f9a31e2c02eda91d193503684fe03772c
Melissa Huang <melissa.huang@berkeley.edu>
2017-03-13 22:05:18 -0700

    changed settings

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-import flask
-
+import flask, os
 app = flask.Flask(__name__)
 
-#renders index.html at route "/"
 @app.route("/")
-def hello(): 
-	return flask.render_template("index.html")
+def hello():
+    return flask.render_template("index.html")
 
+#@app.route("/test")
+#def test():
+#    return flask.render_template("test.html")
 
-if __name__== "__main__": 
-	app.run()
+if __name__ == "__main__":
+    # app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
